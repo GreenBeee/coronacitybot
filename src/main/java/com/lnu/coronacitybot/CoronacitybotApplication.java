020e4443a2d516b8b646ba611a80bef4aeb62728
Oleh Marych <marycholeg@gmail.com>
2020-05-05 14:43:08 +0300

    Initial commit

---
+++ src/main/java/com/lnu/coronacitybot/CoronacitybotApplication.java
@@ -0,0 +1,13 @@
+package com.lnu.coronacitybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronacitybotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronacitybotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lnu/coronacitybot/CoronacitybotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnu.coronacitybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronacitybotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
