5b7d9d2051e52dd43ff2c9f7563c1a96425b3432
Oleh Marych <marycholeg@gmail.com>
2021-06-10 18:09:02 +0300

    added default locale

--- src/main/java/com/lnu/coronacitybot/service/impl/UserServiceImpl.java
+++ src/main/java/com/lnu/coronacitybot/service/impl/UserServiceImpl.java
@@ -19,67 +19,74 @@ import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Value("${facebook.profile.url}")
-    private String profileUrl;
-    @Value("${facebook.token}")
-    private String TOKEN;
-    @Value("${facebook.unlink.url}")
-    private String UNLINK_URL;
-    @Autowired
-    private UserDAO userDAO;
-    @Autowired
-    private RestTemplate restTemplate;
+	@Value("${facebook.profile.url}")
+	private String profileUrl;
+	@Value("${facebook.token}")
+	private String TOKEN;
+	@Value("${facebook.unlink.url}")
+	private String UNLINK_URL;
+	@Autowired
+	private UserDAO userDAO;
+	@Autowired
+	private RestTemplate restTemplate;
 
-    @Override
-    public User createIfNotExist(UserId chatId) {
-        User user = userDAO.findByChatId(chatId.getId());
-        if (user == null) {
-            user = new User();
-            user.setChatId(chatId.getId());
-            user.setState(State.INITIAL);
-            user.setIsSubscribed(Boolean.FALSE);
-            ProfileInfo profileInfo = getProfileInfo(chatId.getId());
-            user.setFirstName(profileInfo.getFirstName());
-            user.setLastName(profileInfo.getLastName());
-            if (!StringUtils.isEmpty(profileInfo.getLocale()))
-                user.setLocale(profileInfo.getLocale());
-            userDAO.save(user);
-        }
-        return user;
-    }
+	@Override
+	public User createIfNotExist(UserId chatId) {
+		User user = userDAO.findByChatId(chatId.getId());
+		if (user == null) {
+			user = new User();
+			user.setChatId(chatId.getId());
+			user.setState(State.INITIAL);
+			user.setIsSubscribed(Boolean.FALSE);
+			ProfileInfo profileInfo = getProfileInfo(chatId.getId());
+			user.setFirstName(profileInfo.getFirstName());
+			user.setLastName(profileInfo.getLastName());
+			if (!StringUtils.isEmpty(profileInfo.getLocale()))
+				user.setLocale(profileInfo.getLocale());
+			userDAO.save(user);
+		} else if (user.getLocale() == null) {
+			ProfileInfo profileInfo = getProfileInfo(chatId.getId());
+			if (!StringUtils.isEmpty(profileInfo.getLocale()))
+				user.setLocale(profileInfo.getLocale());
+			else {
+				user.setLocale("en_US");
+			}
+		}
+		return user;
+	}
 
-    public void unlinkUserAccount(String id) {
-        Psid psid = new Psid(id);
-        restTemplate.postForObject(UNLINK_URL + TOKEN, psid, String.class);
-    }
+	public void unlinkUserAccount(String id) {
+		Psid psid = new Psid(id);
+		restTemplate.postForObject(UNLINK_URL + TOKEN, psid, String.class);
+	}
 
-    public Optional<String> saveLoginAndPasswordAndGetAuthCode(String username, String password, String userID) {
-        //process username and password and return authorization code if successful
-        return Optional.empty();
-    }
+	public Optional<String> saveLoginAndPasswordAndGetAuthCode(String username, String password, String userID) {
+		//process username and password and return authorization code if successful
+		return Optional.empty();
+	}
 
-    private ProfileInfo getProfileInfo(String userId) {
-        String url = profileUrl.replace("$user_id$", userId) + TOKEN;
-        return restTemplate.getForObject(url, ProfileInfo.class);
-    }
+	private ProfileInfo getProfileInfo(String userId) {
+		String url = profileUrl.replace("$user_id$", userId) + TOKEN;
+		return restTemplate.getForObject(url, ProfileInfo.class);
+	}
 
-    public void save(User user) {
-        userDAO.save(user);
-    }
+	public void save(User user) {
+		userDAO.save(user);
+	}
 
-    @Override
-    public void setState(User user, String state) {
-        user.setState(state);
-        save(user);
-    }
+	@Override
+	public void setState(User user, String state) {
+		user.setState(state);
+		save(user);
+	}
 
-    @Override
-    public Set<User> getAllUsersWithActiveSubscription() {
-        return userDAO.findAllByIsSubscribed(Boolean.TRUE);
-    }
+	@Override
+	public Set<User> getAllUsersWithActiveSubscription() {
+		return userDAO.findAllByIsSubscribed(Boolean.TRUE);
+	}
 
-    @Override
-    public void saveAll(Set<User> allUsersWithActiveSubscription) {
-        userDAO.save(allUsersWithActiveSubscription);
-    }
+	@Override
+	public void saveAll(Set<User> allUsersWithActiveSubscription) {
+		userDAO.save(allUsersWithActiveSubscription);
+	}
 }
